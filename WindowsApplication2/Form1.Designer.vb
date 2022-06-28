46fa1f9fc4b471138b248c2e84eadfe551eb78b0
Hanif <99734986+etziv@users.noreply.github.com>
2022-06-28 10:34:24 +0700

    Add files via upload

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,423 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.kode = New System.Windows.Forms.TextBox()
+        Me.nama = New System.Windows.Forms.TextBox()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.price = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.qty = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lv = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.totprice = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.pay = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.bck = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(31, 100)
+        Me.label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(102, 20)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Kode Barang"
+        '
+        'kode
+        '
+        Me.kode.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kode.Location = New System.Drawing.Point(176, 100)
+        Me.kode.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.kode.Name = "kode"
+        Me.kode.Size = New System.Drawing.Size(82, 26)
+        Me.kode.TabIndex = 1
+        '
+        'nama
+        '
+        Me.nama.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nama.Location = New System.Drawing.Point(176, 142)
+        Me.nama.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.nama.Name = "nama"
+        Me.nama.ReadOnly = True
+        Me.nama.Size = New System.Drawing.Size(211, 26)
+        Me.nama.TabIndex = 3
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(31, 142)
+        Me.label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(107, 20)
+        Me.label2.TabIndex = 2
+        Me.label2.Text = "Nama Barang"
+        '
+        'price
+        '
+        Me.price.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.price.Location = New System.Drawing.Point(176, 184)
+        Me.price.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.price.Name = "price"
+        Me.price.ReadOnly = True
+        Me.price.Size = New System.Drawing.Size(150, 26)
+        Me.price.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(31, 184)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Harga"
+        '
+        'qty
+        '
+        Me.qty.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.qty.Location = New System.Drawing.Point(176, 225)
+        Me.qty.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.qty.Name = "qty"
+        Me.qty.Size = New System.Drawing.Size(59, 26)
+        Me.qty.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(31, 225)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Jumlah"
+        '
+        'lv
+        '
+        Me.lv.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.lv.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5})
+        Me.lv.FullRowSelect = True
+        Me.lv.GridLines = True
+        Me.lv.HideSelection = False
+        Me.lv.Location = New System.Drawing.Point(30, 320)
+        Me.lv.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.lv.Name = "lv"
+        Me.lv.Size = New System.Drawing.Size(432, 164)
+        Me.lv.TabIndex = 8
+        Me.lv.UseCompatibleStateImageBehavior = False
+        Me.lv.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Kode"
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Nama Barang"
+        Me.ColumnHeader2.Width = 124
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Harga"
+        Me.ColumnHeader3.Width = 96
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Jumlah"
+        Me.ColumnHeader4.Width = 54
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Total Harga"
+        Me.ColumnHeader5.Width = 94
+        '
+        'totprice
+        '
+        Me.totprice.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.totprice.Location = New System.Drawing.Point(532, 320)
+        Me.totprice.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.totprice.Name = "totprice"
+        Me.totprice.ReadOnly = True
+        Me.totprice.Size = New System.Drawing.Size(174, 44)
+        Me.totprice.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(528, 292)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(137, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Total Pembayaran"
+        '
+        'pay
+        '
+        Me.pay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pay.Location = New System.Drawing.Point(567, 389)
+        Me.pay.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.pay.Name = "pay"
+        Me.pay.Size = New System.Drawing.Size(95, 26)
+        Me.pay.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(476, 396)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(50, 20)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Bayar"
+        '
+        'bck
+        '
+        Me.bck.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bck.Location = New System.Drawing.Point(567, 444)
+        Me.bck.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.bck.Name = "bck"
+        Me.bck.ReadOnly = True
+        Me.bck.Size = New System.Drawing.Size(139, 26)
+        Me.bck.TabIndex = 14
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(476, 448)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(83, 20)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Kembalian"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(54, 276)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 35)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Tambah"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(283, 100)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(59, 28)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Cari"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(201, 276)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(91, 35)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "Hitung"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Ink Free", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(23, 12)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(219, 46)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "KAZIER.CO"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Ink Free", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(65, 58)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(116, 16)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = """ SIMPLE KASIR """
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(467, 325)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(63, 33)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "RP,"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(351, 276)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(91, 35)
+        Me.Button4.TabIndex = 21
+        Me.Button4.Text = "Clear"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(628, 12)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(91, 25)
+        Me.Button5.TabIndex = 22
+        Me.Button5.Text = "Selesai"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(670, 387)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(36, 28)
+        Me.Button6.TabIndex = 23
+        Me.Button6.Text = "✓"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Items.AddRange(New Object() {"1001" & Global.Microsoft.VisualBasic.ChrW(9) & "~ MAKANAN RINGAN", "", "1002" & Global.Microsoft.VisualBasic.ChrW(9) & "~ MAKANAN BERAT", "", "1003" & Global.Microsoft.VisualBasic.ChrW(9) & "~ MINUMAN", "", "1004" & Global.Microsoft.VisualBasic.ChrW(9) & "~ OBAT", "", "1005" & Global.Microsoft.VisualBasic.ChrW(9) & "~ MAINAN", "", "1006" & Global.Microsoft.VisualBasic.ChrW(9) & "~ ALAT MANDI"})
+        Me.ListBox1.Location = New System.Drawing.Point(473, 121)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(233, 124)
+        Me.ListBox1.TabIndex = 24
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(470, 100)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(72, 15)
+        Me.Label11.TabIndex = 25
+        Me.Label11.Text = "Daftar Kode"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(732, 511)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.bck)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.pay)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.totprice)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lv)
+        Me.Controls.Add(Me.qty)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.price)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.nama)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.kode)
+        Me.Controls.Add(Me.label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents label1 As System.Windows.Forms.Label
+    Friend WithEvents kode As System.Windows.Forms.TextBox
+    Friend WithEvents nama As System.Windows.Forms.TextBox
+    Friend WithEvents label2 As System.Windows.Forms.Label
+    Friend WithEvents price As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents qty As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lv As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents totprice As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents pay As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents bck As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+    Dim barang As String
+    Dim Harga As Double
+    Dim totharga As Double
+    Dim total As Double
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If kode.Text = "1001" Then
+            barang = "Makanan Ringan"
+            nama.Text = barang
+            Harga = 5000
+            price.Text = "RP 5.000"
+        ElseIf kode.Text = "1002" Then
+            barang = "Makanan Berat"
+            nama.Text = barang
+            Harga = 10000
+            price.Text = "RP 10.000"
+        ElseIf kode.Text = "1003" Then
+            barang = "Minuman"
+            nama.Text = barang
+            Harga = 3000
+            price.Text = "RP 3.000"
+        ElseIf kode.Text = "1004" Then
+            barang = "Obat-obatan"
+            nama.Text = barang
+            Harga = 7000
+            price.Text = "RP 7.000"
+        ElseIf kode.Text = "1005" Then
+            barang = "Mainan"
+            nama.Text = barang
+            Harga = 15000
+            price.Text = "RP 15.000"
+        ElseIf kode.Text = "1006" Then
+            barang = "Alat Mandi"
+            nama.Text = barang
+            Harga = 13000
+            price.Text = "RP 13.000"
+        ElseIf kode.Text = "" Then
+            MsgBox("Enter The Code !!!", MsgBoxStyle.Information + vbOKOnly, "Perhatian")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If qty.Text = "" Then
+            MsgBox("Enter The Quantity !!!", MsgBoxStyle.Information + vbOKOnly, "Perhatian")
+        ElseIf qty.Text = True Then
+            Dim isi As New ListViewItem
+            isi.Text = kode.Text
+            isi.SubItems.Add(barang)
+            isi.SubItems.Add(price.Text)
+            isi.SubItems.Add(qty.Text)
+            isi.SubItems.Add(CStr(Harga * Val(qty.Text)))
+            lv.Items.Add(isi)
+
+            kode.Text = ""
+            nama.Text = ""
+            price.Text = ""
+            qty.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim GrandTotal As Integer = 0
+        For i As Integer = 0 To lv.Items.Count - 1
+            GrandTotal = GrandTotal + CInt(lv.Items(i).SubItems(4).Text)
+        Next
+        totprice.Text = GrandTotal
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        lv.Items.Clear()
+        totprice.Text = ""
+        pay.Text = ""
+        bck.Text = ""
+
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        bck.Text = "Rp," & CInt(pay.Text) - CInt(totprice.Text)
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Me.Close()
+    End Sub
+
+
+End Class
+
